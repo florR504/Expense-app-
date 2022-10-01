4f2f10635ebb19886ef015e83581a6dd0969304a
Flor <florenciar504@gmail.com>
2022-10-01 20:34:02 -0300

    Seteando 3 estados por separado en un componente

---
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  return (
+    <form action="">
+      <div className="new-expense__controls">
+        <div className="new-expense__controls">
+          <label>Title</label>
+          <input type="text" onChange={titleChangeHandler} />
+        </div>
+        <div className="new-expense__controls">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div className="new-expense__controls">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2022-01-01"
+            max="2025-12-31"
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add expense</button>
+      </div>
+    </form>
+  );
+};
+export default ExpenseForm;
